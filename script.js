170fcc8279ba3441dd6fcd0478bf9d2000ed91e5
PushpendraTyagi57 <krishnatyagi5319@gmail.com>
2025-06-30 10:29:16 +0000

    Game is playable, but still need to add score system and design.

--- script.js
+++ script.js
@@ -15,15 +15,44 @@ function getComputerChoice(max) {
 
 function getHumanChoice() {
     let humanChoice = prompt("Choose: 'Rock', 'Paper' or 'Scissors'");
+    let humanChoiceMain;
     if (humanChoice.toLowerCase() === "rock") {
-        console.log("You chose rock!");
+        humanChoiceMain = "rock";
     }
     else if (humanChoice.toLowerCase() === "paper") {
-        console.log("You chose paper!");
+        humanChoiceMain = "paper";
+    }
+    else if (humanChoice.toLowerCase() === "scissors") {
+        humanChoiceMain = "scissors";
     }
     else {
-        console.log("You chose scissors");
+        console.log("Wrong Choice, Try Again Later!");
     }
+    return humanChoiceMain;
 }
-console.log(getComputerChoice(3));
-console.log(getHumanChoice());
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanSelection, computerSelection) {
+    if (humanSelection === "rock" && computerSelection === "Paper")
+        console.log("Human chose rock, computer chose paper! Paper covers Rock! You lose! :(");
+    if (humanSelection === "rock" && computerSelection === "Scissors")
+        console.log("Human chose rock, computer chose scissors! Rock crushes Scissors! You win!");
+    if (humanSelection === "paper" && computerSelection === "Rock")
+        console.log("Human chose paper, computer chose rock! Paper covers Rock! You win!");
+    if (humanSelection === "paper" && computerSelection === "Scissors")
+        console.log("Human chose paper, computer chose scissors! Scissors cut Paper! You lose! :(")
+    if (humanSelection === "scissors" && computerSelection === "Rock")
+        console.log("Human chose scissors, computer chose rock! Rock crushes Scissors! You lose! :(");
+    if (humanSelection === "scissors" && computerSelection === "Paper")
+        console.log("Human chose scissors, computer chose paper! Scissors cut Paper! You win!");
+    if (humanSelection === computerSelection)
+        console.log("It's a tie!");
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice(3);
+
+playRound(humanSelection, computerSelection);
+
